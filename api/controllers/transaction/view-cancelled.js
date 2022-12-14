088fc01a620520d0bd8cf04bb5b094de6e066489
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2022-12-15 06:29:56 +0700

    feat: change method view by status payment

--- api/controllers/transaction/view-cancelled.js
+++ api/controllers/transaction/view-cancelled.js
@@ -29,50 +29,27 @@ module.exports = {
             let token = credential[1];
             const data = await sails.helpers.decodeJwtToken(token);
 
-            if (data.role !== 'employee' && data.role !== 'admin' && data.role !== 'buyer') {
-                return exits.notRole({
-                    message: 'Dont not access role'
-                });
-            }
-
             let idBuyer = this.req.param('id_buyer');
+            let transactionData
 
-            if (idBuyer) {
-                if(data.role === 'buyer') {
+            if (data.role === 'buyer') {
+                if (idBuyer) {
                     return exits.notRole({
                         message: 'Dont not access role. Only Admin and Employee'
                     });
                 }
-
-                let transactionData = await Transaction.find({ idBuyer: idBuyer, statusPayment: 'cancelled' });
-
-                if (!transactionData) {
-                    return exits.error({
-                        message: 'Transaction not found'
-                    });
-                }
-
-                return exits.success({
-                    message: `Success view Transaction cancelled with id buyer`,
-                    data: transactionData
-                });
-            } else {
-                if(data.role === 'buyer') {
-                    let transactionData = await Transaction.find({idBuyer: data.id, statusPayment: 'cancelled'});
-
-                    return exits.success({
-                        message: `Success view all Transaction cancelled by buyer`,
-                        data: transactionData
-                    });
+                transactionData = await Transaction.find({ idBuyer: data.id, statusPayment: 'cancelled' });
+            } else if (data.role === 'employee' || data.role === 'admin') {
+                if (idBuyer) {
+                    transactionData = await Transaction.findOne({idBuyer: idBuyer, statusPayment: 'cancelled'});
                 } else {
-                    let transactionData = await Transaction.find({statusPayment: 'cancelled'});
-
-                    return exits.success({
-                        message: `Success view all Transaction cancelled by employee or Admin`,
-                        data: transactionData
-                    });
+                    transactionData = await Transaction.find({statusPayment: 'cancelled'});
                 }
             }
+            return exits.success({
+                message: `Success view transaction`,
+                data: transactionData
+            });
         } catch (error) {
             return exits.error({
                 message: 'Something went wrong',
